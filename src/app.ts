6e2f5691030a36f5dbddc017e8163abdaaf6fe6e
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-26 12:21:48 +0100

    Complete 96. Working with Constraints

--- src/app.ts
+++ src/app.ts
@@ -18,9 +18,9 @@
   }
 */
 
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: "Max", hobbies: ['Sports'] }, { age: 30 });
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj);
