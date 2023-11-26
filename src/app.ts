04dae75e89f0b995f765c1cdd431694dea7f21f9
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-26 21:36:41 +0100

    Complete 99. Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergedObj);
 
 interface Lengthy {
@@ -34,15 +34,53 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
   if (element.length === 1) {
     descriptionText = 'Got 1 element.';
   } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.'
+    descriptionText = 'Got ' + element.length + ' elements.';
   }
   return [element, descriptionText];
 }
 
 console.log(countAndDescribe(['Sports', 'Cooking']));
 
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
   return 'Value: ' + obj[key];
 }
 
-extractAndConvert({name: 'Max'}, 'name');
+extractAndConvert({ name: 'Max' }, 'name');
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = {name: 'Max'};
+// objStorage.addItem(maxObj);
+// objStorage.addItem({ name: 'Manu' });
+// // ...
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
